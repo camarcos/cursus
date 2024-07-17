59460446c56c48263cbd2e3f66a6d0c1806604cb
Carolina Marcos Cárdaba <161814384+camarcos@users.noreply.github.com>
2024-07-17 17:01:18 +0200

    Add files via upload

---
+++ get_next_line_bonus.h
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: camarcos <camarcos@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/04 12:53:02 by camarcos          #+#    #+#             */
+/*   Updated: 2024/07/04 12:58:53 by camarcos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <stdint.h>
+# include <limits.h>
+
+
+//principal
+char	*ft_joinfree(char *main, char *temp);
+char	*ft_rest(char *main);
+char	*ft_makeline(char *main);
+char	*ft_readall(int fd, char **main);
+char	*get_next_line(int fd);
+
+//utils
+size_t	ft_strlen(const char *c);
+char	*ft_strchr(char *save, int character);
+void	ft_bzero(void *str, unsigned long int n);
+void	*ft_calloc(size_t size, size_t cont);
+char	*ft_strcat(char *s1, char *s2);
+
+#endif
